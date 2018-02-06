12ff3214d82c053823ff81a70496f5e865d09adf
Kartikey Pandey <pandeykartikey99@gmail.com>
2018-02-07 04:57:09 +0530

    UI for station selection done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var entry = require('./routes/entry');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/entry', entry);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,30 +1,42 @@
 var map;
-
+var pushpin = [];
+var index = 0;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
 	addPushPin({
 		latitude: 29.94879913330079,
 		longitude: 77.89939880371088,
 		num: '12231'
-	})
+	});
 }
 
 // train = {latitude, longitude, num}
 function addPushPin(train) {
-    var pushpin = new Microsoft.Maps.Pushpin(
+    pushpin.push(new Microsoft.Maps.Pushpin(
     { 
     	latitude: train.latitude,
     	longitude: train.longitude 
     },
 	{ 
-		icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png', 
-		anchor: new Microsoft.Maps.Point(12, 39), 
+		icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
 		title: train.num 
-	});
-    map.entities.push(pushpin);
+	}));
+
+    map.entities.push(pushpin[index]);
+    index = index +1;
 }
 
 window.addEventListener('load', 
   function() { 
 	loadMapScenario();
   }, false);
+
+function userInput() {
+	Microsoft.Maps.Events.addHandler(map, 'click', function (mouseEvent) {
+    	//TODO: alert for train number
+    	console.log(mouseEvent.location);
+    	//TODO: Add a pushpin in map
+    	//TODO: Add a pushpin in table
+    	//TODO: Add an on click handler on push pin to remove it 
+	});
+}
